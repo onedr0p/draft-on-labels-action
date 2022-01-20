16cc2765f0c5711e5d1873fad29d420901f53215
Devin Buhl <devin@buhl.casa>
2022-01-20 12:45:27 -0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,109 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+
+function isValidEvent(event: string, action?: string) {
+  const { context } = github
+  const { payload } = context
+  if (event === context.eventName) {
+    return action == null || action === payload.action
+  }
+  return false
+}
+
+function getInputAsArray(
+  name: string,
+  options?: core.InputOptions
+): string[] {
+  return getStringAsArray(core.getInput(name, options))
+}
+
+function getStringAsArray(str: string): string[] {
+  return str
+    .split(/[\n,]+/)
+    .map(s => s.trim())
+    .filter(x => x !== '')
+}
+
+const octokit = github.getOctokit(core.getInput('token'))
+
+async function toDraft(id: string): Promise<void> {
+  await octokit.graphql(
+    `
+    mutation($id: ID!) {
+      convertPullRequestToDraft(input: {pullRequestId: $id}) {
+        pullRequest {
+          id
+          number
+        }
+      }
+    }
+    `,
+    {
+      id
+    }
+  )
+}
+
+async function toReady(id: string): Promise<void> {
+  await octokit.graphql(
+    `
+    mutation($id: ID!) {
+      markPullRequestReadyForReview(input: {pullRequestId: $id}) {
+        pullRequest {
+          id
+          number
+        }
+      }
+    }
+    `,
+    {
+      id
+    }
+  )
+}
+
+const labels = getInputAsArray('labels')
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const { context } = github
+    core.debug(`event: ${context.eventName}`)
+    core.debug(`action: ${context.payload.action}`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (
+      isValidEvent('pull_request_target', 'labeled') ||
+      isValidEvent('pull_request_target', 'unlabeled') ||
+      isValidEvent('pull_request', 'labeled') ||
+      isValidEvent('pull_request', 'unlabeled')
+    ) {
+      
 
-    core.setOutput('time', new Date().toTimeString())
+      let label = context.payload.label.name as string
+      if (context.payload.action === 'unlabeled') {
+        label = `-${label}`
+      }
+
+      core.debug(`Label: ${context.payload.label.name}`)
+
+      if (context.payload.pull_request) {
+        if (context.payload.action === 'labeled') {
+          if (labels.includes(context.payload.label.name)) {
+            await toDraft(context.payload.pull_request?.key)
+            core.info(
+              `Pull Request ${context.payload.pull_request?.number} converted to draft`
+            )
+          }
+        }
+        if (context.payload.action === 'unlabeled') {
+          if (labels.includes(label)) {
+            await toReady(context.payload.pull_request?.key)
+            core.info(
+              `Pull Request ${context.payload.pull_request?.number} ready for review`
+            )
+          }
+        }
+      }
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
